1f00900c260911bbcf3900f00befcf5197d80ce2
BotJacob00 <zhaozhenjill@gmail.com>
2025-03-14 12:17:44 +1100

    added retrieval function, modified gitignore to ignore __pycache__

--- backend/retrieval/query_process.py
+++ backend/retrieval/query_process.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
-
 
 import re
 import spacy
 from sentence_transformers import SentenceTransformer
 import os
-
-
-
-# In[ ]:
-
 
 nlp = spacy.load('en_core_web_sm')
 def preprocess_query(query, lowercase=True):
@@ -24,34 +17,20 @@ def preprocess_query(query, lowercase=True):
     query_tokens = ' '.join([token.text for token in doc if not token.is_stop])
     return query_tokens 
 
-
-# In[2]:
-
-
-query = "  What are the best universities in Australia? "
-pre_processed_query = preprocess_query(query)
-print(pre_processed_query)
-
-
-
-# In[5]:
-
-
 model_path = os.path.abspath("../../sentence_transformer.model")
 Query_processor = SentenceTransformer(model_path)
 
-
-# In[6]:
-
-
-query_vector = Query_processor.encode(pre_processed_query)
-print(query_vector)
-print(query_vector.shape)
-
-
+if __name__ == "__main__":
+    query = "  What are the best universities in Australia? "  ##  Change the query here
+    pre_processed_query = preprocess_query(query)
+    print(pre_processed_query)
+    query_vector = Query_processor.encode(pre_processed_query)
+    print(query_vector)
+    print(query_vector.shape)
 
 
-# In[ ]:
+
+
 
 
 
